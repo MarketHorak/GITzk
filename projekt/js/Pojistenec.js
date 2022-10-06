f3e62642b90d3ef616eeada79f0a72a1e8150e5c
MarketHorak <sagurogodot@gmail.com>
2022-10-06 12:09:29 +0200

    1

---
+++ projekt/js/Pojistenec.js
@@ -0,0 +1,53 @@
+class Pojistenec {
+  constructor() {
+    this.zaznamy = [];
+
+    this.jmenoInput = document.getElementById("jmeno");
+    this.prijmeniInput = document.getElementById("prijmeni");
+    this.vekInput = document.getElementById("vek");
+    this.telefonInput = document.getElementById("telefon");
+
+    this.ulozitButton = document.getElementById("submit");
+    this.vypisElement = document.getElementById("vypisZaznamy");
+    this.nastavUdalosti();
+  /*   this.nasetujZaznam(); */
+/*     this.body=document.querySelector("body") */
+  }
+
+  nastavUdalosti() {
+    this.ulozitButton.onclick = () => {
+      const zaznam = new Zaznam(
+        this.jmenoInput.value,
+        this.prijmeniInput.value,
+        this.vekInput.value,
+        this.telefonInput.value
+      );
+      this.zaznamy.push(zaznam);
+      this.vypisZaznamy();
+      this.nasetujZaznam();
+    };
+  }
+
+  vypisZaznamy() {
+    this.vypisElement.innerHTML = "";
+    for (let i = 0; i < this.zaznamy.length; i++) {
+      const zaznam = this.zaznamy[i];
+    
+  
+    this.vypisElement.innerHTML +=/* this.vypisElement.innerHTML */  `<h3>${zaznam.jmeno} ${zaznam.prijmeni} </h3><h5>Vek:${zaznam.vek}  </br> Telefoni cislo:${zaznam.telefon}</h5>`;
+/*    this.vypisElement.appendChild(zaznamA)
+/*  */   /* this.vypisElement.appendChild(nadpis) */
+   
+    }
+  }
+  nasetujZaznam(){
+    this.jmenoInput.value="";
+        this.prijmeniInput.value="";
+        this.vekInput.value="";
+        this.telefonInput.value="";
+  };
+}
+/* let nadpis = document.createElement("h3")
+nadpis.innerHTML =/* this.vypisElement.innerHTML   `<h3>${zaznam.jmeno} ${zaznam.prijmeni} </h3></br><h4>Vek:${zaznam.vek}  </br> Telefoni cislo:${zaznam.telefon}</h4>`;
+  this.vypisElement.appendChild(zaznamA)
+  this.vypisElement.appendChild(nadpis) */
---
+++ projekt/js/obsluha.js
@@ -0,0 +1,2 @@
+const pojistenec = new Pojistenec ();
+pojistenec.vypisZaznamy();

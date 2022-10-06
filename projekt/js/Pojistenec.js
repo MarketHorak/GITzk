bc7c8756e702cae140b12e6c02998ed7eb6ed84d
MarketHorak <sagurogodot@gmail.com>
2022-10-06 19:19:51 +0200

    2

--- projekt/js/Pojistenec.js
+++ projekt/js/Pojistenec.js
@@ -34,7 +34,8 @@ class Pojistenec {
       const zaznam = this.zaznamy[i];
     
   
-    this.vypisElement.innerHTML +=/* this.vypisElement.innerHTML */  `<h3>${zaznam.jmeno} ${zaznam.prijmeni} </h3><h5>Vek:${zaznam.vek}  </br> Telefoni cislo:${zaznam.telefon}</h5>`;
+    this.vypisElement.innerHTML +=  `<h3>${zaznam.jmeno} ${zaznam.prijmeni} </h3><h5>Vek:${zaznam.vek}  </br> 
+    Telefoni cislo:${zaznam.telefon}</h5><HR size=2 width="80%" align="left" noshade color=", "grey">`;
 /*    this.vypisElement.appendChild(zaznamA)
 /*  */   /* this.vypisElement.appendChild(nadpis) */
    
